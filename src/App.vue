0b7753ab7a5e6aeb7511f7ee3afddf71330d9391
wanghao8080 <117663444@qq.com>
2017-10-18 10:26:39 +0800

    应用的样式

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@
 
 <script>
 import Notes from './components/Notes'
+import 'semantic-ui-css/semantic.min.css'
 
 export default {
   name: 'app',
@@ -16,12 +17,30 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.notes {
+  margin-top: 32px;
+}
+
+.notes .ui textarea {
+  border: none;
+  font-size: 16px;
+  padding: 0;
+  margin: 32px 0;
+}
+
+.ui.items > .item > .content > .header {
+  display: block;
+  cursor: pointer;
+}
+
+.ui.items > .item > .meta {
+  width: 88px;
+  color: rgba(0, 0, 0, 0.4);
+  margin-top: 0;
+  margin-bottom: 16px;
+}
+
+.trash.icon {
+  cursor: pointer;
 }
 </style>
